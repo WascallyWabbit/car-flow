505e409b00549524a4dfefca93ffabacbafac4cd
WascallyWabbit <eric.fowler@gmail.com>
2017-10-18 20:36:11 -0700

    Add files via upload

--- classifier.py
+++ classifier.py
@@ -4,6 +4,8 @@ import os
 from PIL import  Image
 import random
 import matplotlib.pyplot as plt
+import sys
+import argparse
 
 file_path_test = '/Users/Eric Fowler/Downloads/carvana/test/'
 file_path_train = '/Users/Eric Fowler/Downloads/carvana/train/'
@@ -92,7 +94,7 @@ def read_image(path, fname, show, scale=1.0, crop=True):
    mma = mma.flatten('F')
    return mma
 #train(tr_list=zz[0],train_step=train_step,epochs=EPOCHS,numclasses=NUMCLASSES,sess=sess,x=x,y_=y_,crop=CROP,show=SHOW,scale=SCALE)
-def train(train_step, sess, tr_list,x,y_,epochs,numclasses,show,crop,scale=1.0):
+def train(train_step, sess, tr_list,x,y_,epochs,numclasses,show,crop,filepath,scale=1.0):
     idx=0
     for iter in range(epochs):
         train_features = []
@@ -106,7 +108,7 @@ def train(train_step, sess, tr_list,x,y_,epochs,numclasses,show,crop,scale=1.0):
                 if idx >= 0 and idx < numclasses:
                     showChild = True and show
 
-            train_features.append(read_image(file_path_mnist_train, filename, scale=scale, show=showChild,crop=crop))
+            train_features.append(read_image(filepath, filename, scale=scale, show=showChild,crop=crop))
             train_labels.append(labels_onehot)
             #take # of pixels from size of 1st image
             if idx == 0:
@@ -116,7 +118,7 @@ def train(train_step, sess, tr_list,x,y_,epochs,numclasses,show,crop,scale=1.0):
         train_step.run(feed_dict={x: train_features, y_: train_labels})
 
 
-def test(tt_list, sess, accuracy,x,y_,scale,crop,show):
+def test(tt_list, sess, accuracy,x,y_,filepath,scale,crop,show):
     idx = 0
 
     test_features = []
@@ -125,7 +127,7 @@ def test(tt_list, sess, accuracy,x,y_,scale,crop,show):
         if idx % 100 == 0:
             print('Testing:', idx)
 
-        test_features.append(read_image(file_path_mnist_train, filename, scale=scale, crop=crop,show=show))
+        test_features.append(read_image(filepath, filename, scale=scale, crop=crop,show=show))
         test_labels.append(labels_onehot)
         idx += 1
 
@@ -157,7 +159,8 @@ def make_graph(numpixels, numclasses):
     #train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
     train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
-    sess = tf.InteractiveSession()
+    #sess = tf.InteractiveSession()
+    sess = tf.InteractiveSession(config = tf.ConfigProto(log_device_placement=True))
     tf.global_variables_initializer().run(session=sess)
 
     correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
@@ -165,31 +168,82 @@ def make_graph(numpixels, numclasses):
 
     return x,y,y_,train_step,sess,accuracy
 
+def parseArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--datapath', type=str,
+                        default='/users/eric fowler/downloads/carvana/train/',
+                        help='Directory for storing input data')
+    parser.add_argument('--target', type=str,
+                        default='carvana',
+                        choices=['carvana', 'mnist'],
+                        help='Carvana or MNIST?')
+    parser.add_argument('--env', type=str,
+                        default='pc',
+                        choices=['pc', 'aws'],
+                        help='pc or aws?')
+    parser.add_argument('--sample', type=str,
+                        default='0cdf5b5d0ce1_01.jpg',
+                        help='Sample image file for sizing feature tensor')
+    parser.add_argument('--numclasses', type=int,
+                        default=16,
+                        help='Carvana=16, MNIST=10')
+    parser.add_argument('--images', type=int,
+                        default=200000,
+                        help='Number of images to train')
+    parser.add_argument('--crop', type=bool,
+                        default=True,
+                        help='Crop images for speed?')
+    parser.add_argument('--show', type=bool,
+                        default=False,
+                        help='Show some images?')
+    parser.add_argument('--scale', type=float,
+                        default=1.0,
+                        help='Scaling factor for images')
+    parser.add_argument('--epochs', type=int,
+                        default=1,
+                        help='Epochs')
+    parser.add_argument('--chunks', type=int,
+                        default=20,
+                        help='Cut samples into this many chunks')
+    return parser.parse_known_args()
+
 def main():
-    NUMDIGITS = 10
-    NUMCLASSES = NUMDIGITS
-    CROP = False
-    SHOW=False
-#    NUMCLASSES = 16
-    NUMPIXELS = -1
-    SCALE = 1.0
-    EPOCHS = 1
-    tr=get_mnist_tensor_list(numclasses=NUMCLASSES,path=file_path_mnist_train,num=200000)
-    random.shuffle(tr)
-    tr_len = int(len(tr))
-    tr_list = tr[:int(7*tr_len/8)]
-    tt_list = tr[int(7*tr_len/8):]
-    tr=None
-    NUMPIXELS=get_pixels(crop=CROP,filename=SAMPLE_FILE_MNIST)
+    FLAGS, unparsed = parseArgs()
+
+    TARGET = FLAGS.target
+    ENVIRONMENT=FLAGS.env
+    NUMCLASSES = FLAGS.numclasses
+    CROP = FLAGS.crop
+    SHOW=FLAGS.show
+    SCALE = FLAGS.scale
+    EPOCHS = FLAGS.epochs
+    CHUNKS = FLAGS.chunks
+    DATAPATH=FLAGS.datapath
+    SAMPLE_FILE = DATAPATH + FLAGS.sample
+    NUMPIXELS = get_pixels(crop=CROP, filename=SAMPLE_FILE)
+
+    IMAGES=FLAGS.images
+
+    tensor_list = None
+    if TARGET == 'mnist':
+        tensor_list=get_mnist_tensor_list(numclasses=NUMCLASSES,path=DATAPATH,num=IMAGES)
+    elif TARGET== 'carvana':
+        tensor_list = get_tensor_list(numclasses=NUMCLASSES, path=DATAPATH, num=IMAGES)
+
+    random.shuffle(tensor_list)
+    tensor_list_len = int(len(tensor_list))
+    training_list = tensor_list[:int(7*tensor_list_len/8)]
+    testing_list = tensor_list[int(7*tensor_list_len/8):]
+    tensor_list=None
 
     x,y,y_,train_step,sess,accuracy=make_graph(NUMPIXELS,NUMCLASSES)
 
-    trainer = [tr_list[i:i+len(tr_list)//20] for i in range(0,len(tr_list),len(tr_list)//20)]
-    tester=[tt_list[i:i+len(tt_list)//20] for i in range(0,len(tt_list),len(tt_list)//20)]
-    z= zip(trainer,tester)
-    for zz in z:
-        train(tr_list=zz[0],train_step=train_step,epochs=EPOCHS,numclasses=NUMCLASSES,sess=sess,x=x,y_=y_,crop=CROP,show=SHOW,scale=SCALE)
-        test(tt_list=zz[1],sess=sess,accuracy=accuracy,x=x,y_=y_,scale=SCALE, crop=CROP,show=SHOW)
+    trainer = [training_list[i:i+len(training_list)//CHUNKS] for i in range(0,len(training_list),len(training_list)//CHUNKS)]
+    tester=[testing_list[i:i+len(testing_list)//CHUNKS] for i in range(0,len(testing_list),len(testing_list)//CHUNKS)]
+
+    for chunk in zip(trainer,tester):
+        train(tr_list=chunk[0],train_step=train_step,epochs=EPOCHS,numclasses=NUMCLASSES,sess=sess,x=x,y_=y_,crop=CROP,show=SHOW,scale=SCALE,filepath=DATAPATH)
+        test(tt_list=chunk[1],sess=sess,accuracy=accuracy,x=x,y_=y_,scale=SCALE, crop=CROP,show=SHOW,filepath=DATAPATH)
 
 
 
