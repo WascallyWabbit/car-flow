47a0fbc59bb525b1ff1630f4653ad0e3097f3711
WascallyWabbit <eric.fowler@gmail.com>
2017-10-22 21:05:38 -0700

    Add files via upload

--- classifier.py
+++ classifier.py
@@ -25,8 +25,6 @@ def get_pixels(filename=SAMPLE_FILE,crop =True):
     mma = np.array(mm)
     mma = mma.flatten('F')
     return len(mma)
-
-
 def get_mnist_tensor_list(numclasses = 10,path=file_path_mnist_train, num=None):
 
     label_pool=[]
@@ -45,15 +43,10 @@ def get_mnist_tensor_list(numclasses = 10,path=file_path_mnist_train, num=None):
         num = len(files)
 
     return (list(zip(files[:num], labels[:num])))
-
-
-
 def mnist_clean(l,n):
     for file,label in l:
         file=file.rstrip(['0123456789//'])
         file = file + '//'
-
-
 def get_tensor_list(numclasses=16,path='/Users/Eric Fowler/Downloads/carvana/train/', num=None):
 
     files = os.listdir(path)
@@ -72,10 +65,8 @@ def get_tensor_list(numclasses=16,path='/Users/Eric Fowler/Downloads/carvana/tra
         num = len(jpgs)
 
     return (list(zip(jpgs[:num], labels[:num])))
-
 def get_carvana_test_tensor_list(numclasses=16,path='/Users/Eric Fowler/Downloads/carvana/test/', num=None):
     return get_tensor_list(numclasses=numclasses,path=path, num=num)
-
 def read_image(path, fname, show, scale=1.0, crop=True):
    mm = Image.open(path + fname)
    if crop==True:
@@ -96,18 +87,18 @@ def read_image(path, fname, show, scale=1.0, crop=True):
    mma = np.array(mm)
    mma = mma.flatten('F')
    return mma
-#train(tr_list=zz[0],train_step=train_step,epochs=EPOCHS,numclasses=NUMCLASSES,sess=sess,x=x,y_=y_,crop=CROP,show=SHOW,scale=SCALE)
 def train(train_step, sess, tr_list,x,y_,epochs,numclasses,show,crop,filepath,scale=1.0):
     idx=0
-    for iter in range(epochs):
+    print('Training, %d datums, %d epochs' %(len(tr_list), epochs) )
+    for epoch in range(epochs):
         train_features = []
         train_labels = []
+        items = len(tr_list)
         for filename, labels_onehot in tr_list:
             showChild = False and show
             if idx >= 0 and idx < numclasses:
                 showChild = True and show
-            if idx % 100 == 0:
-                print('Training:', idx,iter)
+
                 if idx >= 0 and idx < numclasses:
                     showChild = True and show
 
@@ -119,24 +110,20 @@ def train(train_step, sess, tr_list,x,y_,epochs,numclasses,show,crop,filepath,sc
 
             idx+=1
         train_step.run(feed_dict={x: train_features, y_: train_labels})
-
-
 def test(tt_list, sess, accuracy,x,y_,filepath,scale,crop,show):
     idx = 0
     results = []
     test_features = []
     test_labels = []
+    items = len(tt_list)
     for filename, labels_onehot in tt_list:
-        if idx % 100 == 0:
-            print('Testing:', idx)
-
         test_features.append(read_image(filepath, filename, scale=scale, crop=crop,show=show))
         test_labels.append(labels_onehot)
         idx += 1
 
     test_result = sess.run(accuracy, feed_dict={x: test_features, y_: test_labels})
 
-    print('Testing:%d (%02.12f)' % (idx - 1, test_result))
+    print('Testing:%d items, result (%02.12f)' % (len(tt_list), test_result))
     results.append(test_result)
     return results
 #
@@ -145,17 +132,12 @@ def test(tt_list, sess, accuracy,x,y_,filepath,scale,crop,show):
 def make_graph(numpixels, numclasses, minimize='cross', train_step='sgd'):
     x = tf.placeholder(tf.float32, [None,numpixels])
 
-
     #variables for computation
     #2d array of weights,[pixels, classes]
-    #W = tf.Variable(tf.zeros([numpixels,numclasses]))
     W= tf.get_variable("W",initializer=tf.zeros([numpixels,numclasses]))
-    #W = tf.Variable(tf.zeros([NUMPIXELS,NUMCLASSES],dtype=tf.float32),dtype=tf.float32)
 
     #1d array of bias vars
-    #b = tf.Variable(tf.zeros(numclasses))
     b = tf.get_variable("b",initializer=tf.zeros(numclasses))
-    #b = tf.Variable(tf.zeros(NUMCLASSES,dtype=tf.float32),dtype=tf.float32)
     #the array of 'answers' produced by fxn. of W, x & b, a 1xNUMCLASSES array
     y = tf.nn.softmax(tf.matmul(x, W) + b,name="softmaxxx")
 
@@ -172,7 +154,6 @@ def make_graph(numpixels, numclasses, minimize='cross', train_step='sgd'):
     elif train_step == 'adam':
         train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
-    #sess = tf.InteractiveSession()
     sess = tf.InteractiveSession(config = tf.ConfigProto(log_device_placement=True))
     tf.global_variables_initializer().run(session=sess)
 
@@ -185,10 +166,9 @@ def make_graph(numpixels, numclasses, minimize='cross', train_step='sgd'):
 def pickle_results(test_csv, results):
 
     with open(test_csv, 'w') as fp:
-        writer = csv.writer(fp,dialect=csv.unix_dialect)
+        writer = csv.writer(fp)
         for r in results:
             writer.writerow(r)
-
 def parseArgs():
     parser = argparse.ArgumentParser()
     parser.add_argument('--train_data_path', type=str,
@@ -279,7 +259,7 @@ def main():
         tensor_list = get_tensor_list(numclasses=NUMCLASSES, path=TRAIN_DATA_PATH, num=NUM_TRAIN_IMAGES)
 
     random.shuffle(tensor_list)
-    tensor_list_len = int(len(tensor_list))
+#    tensor_list_len = int(len(tensor_list))
     training_list = tensor_list[:]
 #    testing_list = tensor_list[int(7*tensor_list_len/8):]
     testing_list = get_carvana_test_tensor_list(path=TEST_DATA_PATH, num=NUM_TEST_IMAGES)
